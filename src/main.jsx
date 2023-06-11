d7e144e977dbe24de1b29ffd02cc1052872b83fd
arifremal <arifremal013@gmail.com>
2023-06-11 17:56:42 +0600

    another hook created to added course in the datbase

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,23 @@ import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { router } from "./Routes/Routes.jsx";
 import AuthProvider from "./Authproviders/AuthProvider.jsx";
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
   <AuthProvider>
+  <QueryClientProvider client={queryClient}>
   <div className="max-w-screen-xl mx-auto">
     <RouterProvider router={router} />
     </div>
+ </QueryClientProvider>
   </AuthProvider>
   </React.StrictMode>
 );
