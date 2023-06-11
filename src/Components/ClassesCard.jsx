94ae2435394882dd27f2cdd0bac78a61a4dfc3be
arifremal <arifremal013@gmail.com>
2023-06-11 16:48:40 +0600

    successfully data loader done from database for classes page

--- src/Components/ClassesCard.jsx
+++ src/Components/ClassesCard.jsx
@@ -1,10 +1,21 @@
-import React from "react";
+import { useContext } from "react";
+import { AuthContext } from "../Authproviders/AuthProvider";
 
 const ClassesCard = ({ item }) => {
-  const handleAddtoCart =item =>{
+  const {user} = useContext(AuthContext)
+  const handleAddtoCart = (item) => {
     console.log(item);
-    
-  }
+    if(user){
+      fetch('http://localhost:5000/enrollled')
+      .then(res=> res.json())
+      .then(data =>{
+        if(data.insertedId){
+          
+        }
+
+      })
+    }
+  };
 
   const { image, name, InsturorName, email, price, seats } = item;
   return (
@@ -18,15 +29,17 @@ const ClassesCard = ({ item }) => {
           <p className="">{InsturorName}</p>
           <p className="">Email: {email}</p>
           <div className="flex justify-between ">
-           
-              <p className="me-10">seats: {seats}</p>
-            
-         
-              <p className="">Price: {price}</p>
-           
+            <p className="me-10">seats: {seats}</p>
+
+            <p className="">Price: {price}</p>
           </div>
           <div className="card-actions">
-            <button onClick={()=>handleAddtoCart(item)} className="btn  bg-yellow-400 text-black ">Enroll Now</button>
+            <button
+              onClick={() => handleAddtoCart(item)}
+              className="btn  bg-yellow-400 text-black "
+            >
+              Enroll Now
+            </button>
           </div>
         </div>
       </div>
